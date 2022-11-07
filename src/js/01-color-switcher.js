c9d50c6bfd7f48455cc3647e8f160b01b75873ac
CyresSmith <107186155+CyresSmith@users.noreply.github.com>
2022-11-07 22:42:27 +0200

    Color switcher is done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const refs = {
+    startBtn: document.querySelector('[data-start]'),
+    stopBtn: document.querySelector('[data-stop]'),
+    body: document.querySelector('body')
+}
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const addBodyBackgroundColor = () => refs.body.setAttribute('style', `background-color:${getRandomHexColor()}`);
+
+let intervalID;
+
+const toggleBodyBackgroundColorChenge = (e) => {
+
+  switch (e.target) {  
+
+    case refs.startBtn:
+      refs.startBtn.disabled = true;
+      intervalID = setInterval(addBodyBackgroundColor, 1000)
+      break;
+    
+    case refs.stopBtn:
+      refs.startBtn.disabled = false;
+      clearInterval(intervalID)
+      break;
+  }
+};
+
+refs.body.addEventListener('click', toggleBodyBackgroundColorChenge);
